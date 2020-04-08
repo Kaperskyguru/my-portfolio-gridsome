3718d29770f019c0f758005d86243262eedef410
kaperskyguru <solomoneseme@gmail.com>
2020-04-08 23:47:48 +0100

    Added styles and fix errors

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from "~/layouts/Default.vue";
-import "~/assets/open-iconic-bootstrap.min.css";
-import "~/assets/animate.css";
-import "~/assets/owl.carousel.min.css";
-import "~/assets/owl.theme.default.min.css";
-import "~/assets/magnific-popup.css";
-import "~/assets/aos.css";
+// import "~/assets/open-iconic-bootstrap.min.css";
+// import "~/assets/animate.css";
+// import "~/assets/owl.carousel.min.css";
+// import "~/assets/owl.theme.default.min.css";
+// import "~/assets/magnific-popup.css";
+// import "~/assets/aos.css";
 
-import "~/assets/ionicons.min.css";
+// import "~/assets/ionicons.min.css";
 import "~/assets/flaticon.css";
-import "~/assets/icomoon.css";
+// import "~/assets/icomoon.css";
 
 import "~/assets/style.css";
 
@@ -29,5 +29,51 @@ export default function(Vue, { router, head, isClient }) {
     rel: "stylesheet",
     href:
       "https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900",
+  });
+
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css",
+  });
+
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css",
+  });
+
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css",
+  });
+
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css",
+  });
+
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css",
+  });
+
+  head.link.push({
+    rel: "stylesheet",
+    href: "https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css",
+  });
+
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.0.0-19/css/ionicons.min.css",
+  });
+
+  head.link.push({
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/npm/icomoon@1.0.0/style.css",
   });
 }
